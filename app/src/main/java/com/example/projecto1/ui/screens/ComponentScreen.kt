f2a1239e01e763bf7bb3ed5d78c5e70f660ea635
DamianGaelJR <gaeljimenez463@gmail.com>
2024-09-30 08:33:09 -0600

    component Screen 4

--- app/src/main/java/com/example/projecto1/ui/screens/ComponentScreen.kt
+++ app/src/main/java/com/example/projecto1/ui/screens/ComponentScreen.kt
@@ -1,16 +1,24 @@
 package com.example.projecto1.ui.screens
+
 import android.widget.Button
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExtendedFloatingActionButton
@@ -19,12 +27,17 @@ import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.InputChip
+import androidx.compose.material3.InputChipDefaults
 import androidx.compose.material3.LargeFloatingActionButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Slider
 import androidx.compose.material3.SmallFloatingActionButton
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDrawerState
@@ -38,6 +51,8 @@ import androidx.navigation.NavController
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.semantics.Role.Companion.Checkbox
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import kotlinx.coroutines.launch
 import org.w3c.dom.Text
@@ -157,7 +172,7 @@ fun FloatingButtons() {
         )
     }
 }
-
+@Preview(showBackground = true)
 @Composable
 fun Chips() {
     Column (
@@ -189,6 +204,125 @@ fun Chips() {
                 }
             } else {
                 null
+            }
+        )
+    }
+}
+@Composable
+fun InputChipsExample(
+    text: String,
+    onDismiss: () -> Unit
+){
+    var enabled by remember { mutableStateOf(true)}
+    if (!enabled) return
+
+    InputChip(
+        label = { Text(text) },
+        selected = enabled,
+        onClick = { onDismiss()
+            enabled = !enabled
+        },
+        avatar = {
+            Icon(
+                Icons.Filled.Person,
+                contentDescription = "",
+                Modifier.size(InputChipDefaults.AvatarSize)
+
+            )
+        },
+        trailingIcon = {
+            Icon(
+                Icons.Filled.Close,
+                contentDescription = "",
+                Modifier.size(InputChipDefaults.AvatarSize)
+            )
+        }
+
+    )
+}
+
+@Composable
+fun Progress() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        LinearProgressIndicator(
+            modifier = Modifier.fillMaxWidth()
+        )
+        CircularProgressIndicator(
+            modifier = Modifier.width(64.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Sliders() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        var sliderPosition by remember { mutableStateOf(50f)}
+        Column {
+            Slider(
+                value = sliderPosition,
+                onValueChange = { sliderPosition = it },
+                steps = 10,
+                valueRange = 0f..100f
+            )
+            Text(
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth(),
+                text = "Slider position: $sliderPosition"
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Switches() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        var checked by remember { mutableStateOf(true)}
+        Switch(
+            checked = checked,
+            onCheckedChange = {
+                checked = it
+            }
+        )
+        var checked2 by remember { mutableStateOf(true)}
+        Switch(
+            checked = checked2,
+            onCheckedChange = {
+                checked2 = it
+            },
+            thumbContent = if (checked2) {
+                {
+                    Icon(
+                        Icons.Filled.Check,
+                        contentDescription = "",
+                        Modifier.size(InputChipDefaults.AvatarSize)
+                    )
+                }
+            }else{
+                null
+            }
+        )
+        var checked3 by remember { mutableStateOf(true)}
+        Checkbox(
+            checked = checked3,
+            onCheckedChange = {
+                checked3 = it
             }
         )
     }
